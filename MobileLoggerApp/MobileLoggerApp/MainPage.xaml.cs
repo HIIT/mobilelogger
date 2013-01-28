8a1eae2527d04f687f201f0875140234a1821f5b
Ville Kukkola <ville.kukkola@helsinki.fi>
2013-01-28 16:21:24 +0200

    Refactored connection logic.

--- MobileLoggerApp/MobileLoggerApp/MainPage.xaml.cs
+++ MobileLoggerApp/MobileLoggerApp/MainPage.xaml.cs
@@ -1,15 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
+using MobileLoggerApp.src;
 
 namespace MobileLoggerApp
 {
@@ -24,15 +16,8 @@ namespace MobileLoggerApp
         private void sendPostToServer(object sender, RoutedEventArgs e)
         {
            // retrieve an avatar image from the Web
-           string avatarUri = "http://t-jonimake.users.cs.helsinki.fi/MobileLoggerServerDev/postFromPhone";
-           HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(avatarUri);
-           request.Method = "POST";
-           request.BeginGetResponse(responseCallBack, request);
-        }
-
-        private void responseCallBack(IAsyncResult asyncResult)
-        {
-            Console.WriteLine(asyncResult.ToString());
+            Connection.PostTestCase();
+           
         }
     }
 }

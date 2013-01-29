a9a7f80bc3d979518696e31cfb838a56c125c39a
Joni <joni.makela@gmail.com>
2013-01-29 16:14:00 +0200

    Added some rudimentary handler classes

---
+++ MobileLoggerApp/MobileLoggerApp/src/mobilelogger/AbstractLogHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MobileLoggerApp.src.mobilelogger
+{
+    public abstract class AbstractLogHandler
+    {
+
+        public abstract Boolean SendData();
+
+
+        public abstract void HandleSendError();
+
+    }
+}

780419d4901a15bc4684b3a70abf471bea60facb
unknown <kivikant@dhp-79.CSWin.cs.Helsinki.FI>
2013-02-05 16:19:40 +0200

    Database object for logs.

--- MobileLoggerApp/MobileLoggerApp/pages/MainPage.xaml.cs
+++ MobileLoggerApp/MobileLoggerApp/pages/MainPage.xaml.cs
@@ -1,16 +1,54 @@
 ﻿using System;
 using System.Windows;
+using System.Collections.Generic;
 using Microsoft.Phone.Controls;
 using MobileLoggerApp.src;
+using MobileLoggerApp.src.mobilelogger.model;
 
 namespace MobileLoggerApp
 {
+
+
+
     public partial class MainPage : PhoneApplicationPage
     {
+
+        public const string ConnectionString = @"Data Source = 'isostore:/LogEventDB.sdf';";
+
         // Constructor
         public MainPage()
         {
             InitializeComponent();
+
+            using (LogEventDataContext logDBContext = new LogEventDataContext(ConnectionString))
+            {
+
+                if (!logDBContext.DatabaseExists())
+                {
+                    // create database if it does not exist
+                    try
+                    {
+                        logDBContext.CreateDatabase();
+                    }
+                    catch (InvalidOperationException ioe) {
+                        System.Diagnostics.Debug.WriteLine("InvalidOperationException while creating database..."+ioe);
+                    }
+                }
+
+                logDBContext.addEvent(string.Format("Latitude: {0}, Longitude: {1}, Altitude: {2}", 0.1, 0.2, 0.3));
+                
+                IList<LogEvent> list = logDBContext.GetLogEvents();
+                if (list != null)
+                {
+
+                    foreach (LogEvent e in list)
+                    {
+                        System.Diagnostics.Debug.WriteLine(e.ToString()+":"+e.sensorEvent);
+                    }
+                }
+                
+            }
+
         }
 
         private void sendPostToServer(object sender, RoutedEventArgs e)

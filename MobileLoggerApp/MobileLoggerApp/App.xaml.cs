814a2f7082b37b0bed9fad820a6c3228805356e8
J.P <jpsalo@zoho.com>
2013-02-05 13:44:52 +0200

    Pivot.

--- MobileLoggerApp/MobileLoggerApp/App.xaml.cs
+++ MobileLoggerApp/MobileLoggerApp/App.xaml.cs
@@ -22,6 +22,24 @@ namespace MobileLoggerApp
         LogManager logManager;
         GpsHandler gps;
 
+        private static ViewModels.MainViewModel viewModel = null;
+
+        /// <summary>
+        /// A static ViewModel used by the views to bind against.
+        /// </summary>
+        /// <returns>The MainViewModel object.</returns>
+        public static ViewModels.MainViewModel ViewModel
+        {
+            get
+            {
+                // Delay creation of the view model until necessary
+                if (viewModel == null)
+                    viewModel = new ViewModels.MainViewModel();
+
+                return viewModel;
+            }
+        }
+
         /// <summary>
         /// Provides easy access to the root frame of the Phone Application.
         /// </summary>
@@ -86,6 +104,11 @@ namespace MobileLoggerApp
         // This code will not execute when the application is first launched
         private void Application_Activated(object sender, ActivatedEventArgs e)
         {
+            // Ensure that application state is restored appropriately
+            if (!App.ViewModel.IsDataLoaded)
+            {
+                App.ViewModel.LoadData();
+            }
         }
 
         // Code to execute when the application is deactivated (sent to background)

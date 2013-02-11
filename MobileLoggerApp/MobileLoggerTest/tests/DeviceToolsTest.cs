445e4214c4badbe485e3f4f9269acd251289c058
Joni <joni.makela@gmail.com>
2013-02-11 12:00:51 +0200

    - more stuff done on the messagingservice, still need to implement validation of response - implemented SHA1 calculation in device tools

--- MobileLoggerApp/MobileLoggerTest/tests/DeviceToolsTest.cs
+++ MobileLoggerApp/MobileLoggerTest/tests/DeviceToolsTest.cs
@@ -15,5 +15,16 @@ namespace MobileLoggerTest.tests
             Assert.AreEqual(unixtime, DeviceTools.GetUnixTime(dateTime));
             Assert.AreEqual(dateTime, DeviceTools.GetDateTime(unixtime));
         }
+
+        [TestMethod]
+        public void TestCalculateSHA1()
+        {
+            string src = "tamaonsha1hashi";
+            string expectedsha1 = "f0e3fc8f9a3e3d27789482075293c7a6a3a24c06";
+
+            string result = DeviceTools.CalculateSHA1(src);
+            Assert.AreEqual(expectedsha1.ToUpper(), result);
+
+        }
     }
 }

d888fb09134f4f3fd4877bccc38de51477eed4ae
unknown <Annahiet@dhp-80.CSWin.cs.Helsinki.FI>
2013-02-11 10:26:30 +0200

    The base for accelerometer tracking.

---
+++ MobileLoggerApp/MobileLoggerApp/src/mobilelogger/Handlers/AccelHandler.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Devices.Sensors;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace MobileLoggerApp.src.mobilelogger.Handlers
+{
+    public class AccelHandler : AbstractLogHandler
+    {
+        Accelerometer accelerometerwatcher;
+        Message accelData;
+        JObject joAccel;
+
+        public override Boolean SendData()
+        {
+
+            joAccel = new JObject();
+
+            return false;
+        }
+
+        public void startAccelWatcher()
+        {
+            if (accelerometerwatcher == null)
+            {
+                // Instantiate the Accelerometer.
+                accelerometerwatcher = new Accelerometer();
+                //accelerometer.TimeBetweenUpdates = TimeSpan.FromMilliseconds(20);
+                accelerometerwatcher.CurrentValueChanged += new EventHandler<SensorReadingEventArgs<AccelerometerReading>>(accelerometer_CurrentValueChanged);
+            }
+
+        }
+        void accelerometer_CurrentValueChanged(object sender, SensorReadingEventArgs<AccelerometerReading> e)
+        {
+            joAccel.Add("X", (float)e.SensorReading.Acceleration.X);
+            joAccel.Add("Y", (float)e.SensorReading.Acceleration.Y);
+            joAccel.Add("Z", (float)e.SensorReading.Acceleration.Z);
+        }
+    }
+}
+
+
+
+
+    
+
+

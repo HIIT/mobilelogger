2b1b1178eb7d703123003f95e8e92857689d922d
unknown <kivikant@dhp-79.CSWin.cs.Helsinki.FI>
2013-02-19 11:58:06 +0200

    Signed-off-by: unknown <kivikant@dhp-79.CSWin.cs.Helsinki.FI>

---
+++ MobileLoggerApp/MobileLoggerTest/tests/LogDatabaseTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MobileLoggerTest.tests
+{
+    [TestClass]
+    public class LogDatabaseTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

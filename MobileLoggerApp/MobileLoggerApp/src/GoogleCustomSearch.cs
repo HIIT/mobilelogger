a7e7ab3bec719da314d8a7292fc030c99b6685e8
Joni <joni.makela@gmail.com>
2013-04-05 13:29:06 +0300

    Moving stuff around

--- MobileLoggerApp/MobileLoggerApp/src/GoogleCustomSearch.cs
+++ MobileLoggerApp/MobileLoggerApp/src/GoogleCustomSearch.cs
@@ -34,7 +34,7 @@ namespace MobileLoggerApp.src
             searchQuery = query;
             //System.Diagnostics.Debug.WriteLine("Search query is: " + query + " at ElGoog.Search");
             //string that contains required api key and information for google api
-            string uri = String.Format("https://www.googleapis.com/customsearch/v1?key=AIzaSyDC_Y2CPa_zvLfgd09pLPoyd02hhvyaN8c&cx=011471749289680283085:rxjokcqp-ae&q={0}&start={1}", query, page);
+            string uri = String.Format("https://www.googleapis.com/customsearch/v1?key={2}&cx=011471749289680283085:rxjokcqp-ae&q={0}&start={1}", query, page, DeviceTools.apiKey);
             //alternatiivinen hakukone ja api-key, käytetty testaukseen
             //string uri = String.Format("https://www.googleapis.com/customsearch/v1?key=AIzaSyCurZXbVyfaksuWlOaQVys5YwbewaBrtCs&cx=014771188109725738891:bcuskpsruhe&q={0}", query);
 

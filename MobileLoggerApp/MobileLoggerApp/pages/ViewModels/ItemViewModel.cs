50e0a92f130cb61a0b3452a76181ad91485e39ea
J.P <jpsalo@zoho.com>
2013-04-18 13:41:35 +0300

    Changes to ItemViewModel to support preserving and restoring page state.

--- MobileLoggerApp/MobileLoggerApp/pages/ViewModels/ItemViewModel.cs
+++ MobileLoggerApp/MobileLoggerApp/pages/ViewModels/ItemViewModel.cs
@@ -1,40 +1,79 @@
 ﻿using Newtonsoft.Json.Linq;
 using System.ComponentModel;
+using System.Runtime.Serialization;
+using System.ComponentModel;
 
 namespace MobileLoggerApp.pages
 {
+    [DataContract]
     public class ItemViewModel : INotifyPropertyChanged
     {
+        private string _lineOne;
+        [DataMember]
         public string LineOne
         {
-            get;
-            set;
+            get { return _lineOne; }
+            set
+            {
+                _lineOne = value;
+                NotifyPropertyChanged("_lineOne");
+            }
         }
 
+        private string _lineTwo;
+        [DataMember]
         public string LineTwo
         {
-            get;
-            set;
+            get { return _lineTwo; }
+            set
+            {
+                _lineTwo = value;
+                NotifyPropertyChanged("_lineTwo");
+            }
         }
 
+        private string _lineThree;
+        [DataMember]
         public string LineThree
         {
-            get;
-            set;
+            get { return _lineThree; }
+            set
+            {
+                _lineThree = value;
+                NotifyPropertyChanged("_lineThree");
+            }
         }
 
+        private JObject _searchResult;
+        [DataMember]
         public JObject SearchResult
         {
-            get;
-            set;
+            get { return _searchResult; }
+            set
+            {
+                _searchResult = value;
+                NotifyPropertyChanged("_searchResult");
+            }
         }
 
+        private bool _isChecked;
+        [DataMember]
         public bool IsChecked
         {
-            get;
-            set;
+            get { return _isChecked; }
+            set
+            {
+                _isChecked = value;
+                NotifyPropertyChanged("_isChecked");
+            }
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            if (null != PropertyChanged)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
